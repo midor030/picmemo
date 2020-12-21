b33b62e673af9a5d1b69c977df3886bb7325fb17
midor030 <itigodaifuku030@icloud.com>
2020-12-21 16:25:12 +0900

    devise実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,7 @@
 class TweetsController < ApplicationController
-  
+  before_action :set_tweet, only: [:edit, :show]
+  before_action :move_to_index, except: [:index, :show]
+
   def index
     @tweets = Tweet.all
   end
@@ -17,9 +19,30 @@ class TweetsController < ApplicationController
     tweet.destroy
   end
 
+  def edit
+  end
+
+  def update
+    tweet = Tweet.find(params[:id])
+    tweet.update(tweet_params)
+  end
+
+  def show
+  end
+
   private
   def tweet_params
-    params.require(:tweet).parmit(:name, :image, :text)
+    params.require(:tweet).permit(:name, :image, :text)
+  end
+
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 
 end
