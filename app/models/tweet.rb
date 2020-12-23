6447a9520db0db568d9c5712e4fcf76a84bff3cb
midor030 <itigodaifuku030@icloud.com>
2020-12-23 14:34:41 +0900

    仕上げ

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,13 @@
 class Tweet < ApplicationRecord
   validates :text, presence: true
+  belongs_to :user
+  has_many :comments
+
+  def self.search(search)
+    if search != ""
+      Tweet.where('text LILE(?)', "%#{search}%")
+    else
+      Tweet.all
+    end
+  end
 end
